541b10f5aa7a965c6af2a309bf78e8aeeccd0ed2
sttr19 <stastroshko@gmail.com>
2022-08-11 01:52:58 +0300

    feat:task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length;j++) {
+      const config = bracketsConfig[j];
+      if (str[i] === config[0] && str[i+1] === config[1]) {
+        return check(
+          str.split('').filter((elem,index)=> index !=i && index!=i+1).join(''), 
+          bracketsConfig
+        );
+      }
+    }
+  }
+  return str.length == 0;
 }
